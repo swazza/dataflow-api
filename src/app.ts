edb0aafcfd9f76e741b8ff3fced23bf41613ce0f
swarup <rup@swazza.io>
2020-11-21 12:55:25 +0530

    Setup envvars

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express = require("express");
+import bootstrap = require("./bootstrap");
 
+const envVars = bootstrap.getEnvVars();
 const app = express();
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello!");
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+app.listen(envVars.httpPort, () => {
+  console.log(`App listening on port ${envVars.httpPort}`);
 });
